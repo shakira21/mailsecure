2e905f75ecf5d2b10fc5a25b722a9ec0fb53e177
shakira21 <84279372+shakira21@users.noreply.github.com>
2021-06-01 22:48:03 +0530

    Update urls.py

--- securemails/urls.py
+++ securemails/urls.py
@@ -19,10 +19,15 @@ from django.urls import include
 from django.conf.urls import url
 from django.conf.urls.static import static
 from mailapp import views
-
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^mailapp/',include('mailapp.urls')),
     url(r'^$',views.index,name='index'),
     url(r'^logout/$', views.user_logout, name='logout'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
